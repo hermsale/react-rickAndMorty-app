e7d4b06712f37bc7855b5d19f22430a9b4c1b8ff
Alejandro Hugo Herms <hermsale@gmail.com>
2023-05-30 11:20:29 -0300

    se dio formato a los componentes

--- src/App.js
+++ src/App.js
@@ -34,76 +34,76 @@ function App() {
     <React.Fragment>
 
       <Selector
-      selectedValue={selectedValue}
-      setSelectedValue={setSelectedValue}
-      loading={loading}
-      error={error}
-      />
-    
-{selectedValue==='character' && (
-      <SearchBar
-      setSearchValue={setSearchValue}
-      loading={loading}
-      error={error}
-      />
-)
-}
-      <Pagination 
-      prevPage={prevPage}
-      nextPage={nextPage}
-      selectedValue={selectedValue}
-      fetchCharacters={fetchCharacters}
-      fetchEpisodes={fetchEpisodes}
-      error={error}
-      />
-      
-    
-
-    
-       { selectedValue==='character' && (
-        <ListaPersonajes
-
-        onLoading = {() => <Loading />}
-        onError = {() => <Error/>}
-
-        searchedCharacters={searchedCharacters}
+        selectedValue={selectedValue}
+        setSelectedValue={setSelectedValue}
         loading={loading}
         error={error}
-        
-        // render props
-          render = {item => (
-              <Personaje
-                key={item.id}
-                name={item.name}
-                species={item.species}
-                status={item.status}
-                location={item.location.name}
-                img={item.image}
-              />
-           
-              )}
-        />
-      )  
-          }
-    
-
-    {selectedValue==='episode' && (
-      <Episode 
-      episodes={episodes}
       />
 
-    )}
-          
-    <Pagination 
-     prevPage={prevPage}
-     nextPage={nextPage}
-     selectedValue={selectedValue}
-     fetchCharacters={fetchCharacters}
-     fetchEpisodes={fetchEpisodes}
-     error={error}
-    />
+      {selectedValue === 'character' && (
+        <SearchBar
+          setSearchValue={setSearchValue}
+          loading={loading}
+          error={error}
+        />
+      )
+      }
+      <Pagination
+        prevPage={prevPage}
+        nextPage={nextPage}
+        selectedValue={selectedValue}
+        fetchCharacters={fetchCharacters}
+        fetchEpisodes={fetchEpisodes}
+        error={error}
+      />
 
-     
+
+
+
+      {selectedValue === 'character' && (
+        <ListaPersonajes
+
+          onLoading={() => <Loading />}
+          onError={() => <Error />}
+
+          searchedCharacters={searchedCharacters}
+          loading={loading}
+          error={error}
+
+          // render props
+          render={item => (
+            <Personaje
+              key={item.id}
+              name={item.name}
+              species={item.species}
+              status={item.status}
+              location={item.location.name}
+              img={item.image}
+            />
+
+          )}
+        />
+      )
+      }
+
+
+      {selectedValue === 'episode' && (
+        <Episode
+          episodes={episodes}
+        />
+
+      )}
+
+      <Pagination
+        prevPage={prevPage}
+        nextPage={nextPage}
+        selectedValue={selectedValue}
+        fetchCharacters={fetchCharacters}
+        fetchEpisodes={fetchEpisodes}
+        error={error}
+      />
+
+
     </React.Fragment>
   );
 }
